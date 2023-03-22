4e3bbada2b6d879701426cd2282c260895592f8b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2023-03-21 22:05:58 -0500

    staticcheck

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/magefile/mage v1.13.0
-	github.com/mcandre/mage-extras v0.0.7
+	github.com/mcandre/mage-extras v0.0.8
 	mvdan.cc/sh v2.6.4+incompatible
 )
 
--- magefile.go
+++ magefile.go
@@ -74,6 +74,9 @@ func Errcheck() error { return mageextras.Errcheck("-blank") }
 // Nakedret runs nakedret.
 func Nakedret() error { return mageextras.Nakedret("-l", "0") }
 
+// Staticcheck runs staticcheck.
+func Staticcheck() error { return mageextras.Staticcheck() }
+
 // Lint runs the lint suite.
 func Lint() error {
 	mg.Deps(GoVet)
@@ -82,6 +85,7 @@ func Lint() error {
 	mg.Deps(GoImports)
 	mg.Deps(Errcheck)
 	mg.Deps(Nakedret)
+	mg.Deps(Staticcheck)
 	return nil
 }
 
