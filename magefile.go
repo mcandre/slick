b539ac606a863568a0556ff120fa7c887cc94c1d
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2024-10-04 13:26:01 -0500

    s/golint/revive/

--- magefile.go
+++ magefile.go
@@ -76,9 +76,6 @@ func GoFmt() error { return mageextras.GoFmt("-s", "-w") }
 // GoImports runs goimports.
 func GoImports() error { return mageextras.GoImports("-w") }
 
-// GoLint runs golint.
-func GoLint() error { return mageextras.GoLint() }
-
 // GoVet runs default go vet analyzers.
 func GoVet() error { return mageextras.GoVet() }
 
@@ -87,6 +84,9 @@ func Errcheck() error { return mageextras.Errcheck("-blank") }
 
 // Nakedret runs nakedret.
 func Nakedret() error { return mageextras.Nakedret("-l", "0") }
+
+// Revive runs revive.
+func Revive() error { return mageextras.Revive() }
 
 // Shadow runs go vet with shadow checks enabled.
 func Shadow() error { return mageextras.GoVetShadow() }
@@ -110,10 +110,10 @@ func Lint() error {
 	mg.Deps(Deadcode)
 	mg.Deps(GoFmt)
 	mg.Deps(GoImports)
-	mg.Deps(GoLint)
 	mg.Deps(GoVet)
 	mg.Deps(Errcheck)
 	mg.Deps(Nakedret)
+	mg.Deps(Revive)
 	mg.Deps(Shadow)
 	mg.Deps(Staticcheck)
 	mg.Deps(Unmake)
